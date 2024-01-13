232cf223bb88f494ef2e6f43c748da78fb4d3570
omochi11 <152568172+omochi11@users.noreply.github.com>
2024-01-14 08:34:30 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+
+const today = new Date();
+
+const date = () => {
+  console.log(today.getFullYear()+ "年");
+  console.log(today.getMonth()+ "月");
+  console.log(today.getDay()+"日");
+}
+
+date();
+
+
+
