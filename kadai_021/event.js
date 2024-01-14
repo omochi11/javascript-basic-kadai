38af6eab4a0a0ccaf527c755262d1a4202f853d1
omochi11 <152568172+omochi11@users.noreply.github.com>
2024-01-14 15:35:00 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,6 +0,0 @@
-const btn =document.getElementById('btn')
-const text =document.getElementById('text')
-
-
-setTimeout(() =>{btn.addEventListener("click",
-  text.textContent = 'ボタンが押されました')},2000);
